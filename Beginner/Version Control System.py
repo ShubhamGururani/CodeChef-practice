5a4b6dcb5249d9648c7dcc66eb90a01ee2e113b3
Shubham Gururani <32897406+ShubhamGururani@users.noreply.github.com>
2018-03-03 23:19:40 +0530

    Delete Version Control System.py

--- Beginner/Version Control System.py
+++
@@ -1,9 +0,0 @@
-import sys
-import math
- 
-t=int(input())
-for i in range(t):
-    n,m,k=map(int,input().split())
-    a=set(map(int,input().split()))
-    b=set(map(int,input().split()))
-    print(len(a.intersection(b)),n-len(a.union(b)))
