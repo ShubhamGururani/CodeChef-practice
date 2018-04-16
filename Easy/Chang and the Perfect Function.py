83be662ed1edb41b8db9d82845ac08c2e5699183
Shubham Gururani <32897406+ShubhamGururani@users.noreply.github.com>
2018-04-16 18:50:25 +0530

    Add files via upload

--- Easy/Chang and the Perfect Function.py
+++ Easy/Chang and the Perfect Function.py
@@ -1,12 +1,6 @@
-def Perfect(number):
-	number = number ** 0.5
-	if number == int(number):
-		return True
-	return False
-a, b = map(int,input().split())
+from math import sqrt
+a, b = map(int, input().split())
 num = 0
-for i in range(1,a+1):
-	for j in range(1, b+1):
-		if Perfect((i**2) + j):
-			num += 1
+for i in range(1, a):
+    num += int(sqrt((i * i + b)) - i)
 print(num)
